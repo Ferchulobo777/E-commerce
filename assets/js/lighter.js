9f87451d6c4f210aa418534a86eb4b8fba03e3c0
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-02-17 05:03:11 -0300

    update

--- assets/js/lighter.js
+++ assets/js/lighter.js
@@ -1,31 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-	let slider = this.getElementById("brightness");
+	let slider = document.getElementById("brightness");
 	slider.addEventListener("input", adjustSlider);
-	adjustSlider(slider);
-});
-function adjustSlider(e) {
+	// Restore the brightness value from local storage
+	let brightness = localStorage.getItem("brightness");
+	if (brightness !== null) {
+	  slider.value = brightness;
+	  adjustSlider(slider);
+	} else {
+	  adjustSlider(slider);
+	}
+  });
+  
+  function adjustSlider(e) {
 	let body = document.body,
-		switchLightMin = 10,
-		switchLightMax = 100,
-		tar = e.target || e,
-		pct = +tar.value / +tar.max,
-		L1 = pct * (switchLightMax - switchLightMin) + switchLightMin,
-		L2 = L1 - 10,
-		L3 = L1 - 37,
-		L = [L1, L2, L3],
-		thumbHueMin = 0,
-		thumbHueMax = 120,
-		thumbHue = pct * (thumbHueMax - thumbHueMin) + thumbHueMin,
-		thumbSat = 90.4,
-		thumbLight = 44.9,
-		thumbHSL = `${thumbHue},${thumbSat}%,${thumbLight}%`;
-
+	  switchLightMin = 10,
+	  switchLightMax = 100,
+	  tar = e.target || e,
+	  pct = +tar.value / +tar.max,
+	  L1 = pct * (switchLightMax - switchLightMin) + switchLightMin,
+	  L2 = L1 - 10,
+	  L3 = L1 - 37,
+	  L = [L1, L2, L3],
+	  thumbHueMin = 0,
+	  thumbHueMax = 120,
+	  thumbHue = pct * (thumbHueMax - thumbHueMin) + thumbHueMin,
+	  thumbSat = 90.4,
+	  thumbLight = 44.9,
+	  thumbHSL = `${thumbHue},${thumbSat}%,${thumbLight}%`;
+  
 	// update the slider shade
 	L.forEach((light, i) => {
-		if (light < 0) light = 0;
-		body.style.setProperty(`--l${i + 1}`, `hsl(228,9.8%,${light}%)`);
+	  if (light < 0) light = 0;
+	  body.style.setProperty(`--l${i + 1}`, `hsl(228,9.8%,${light}%)`);
 	});
 	// update the thumb icon hue
 	body.style.setProperty(`--p`, `hsl(${thumbHSL})`);
 	body.style.setProperty(`--pT`, `hsla(${thumbHSL},0)`);
-}
+  
+	// Store the current brightness value in local storage
+	localStorage.setItem("brightness", tar.value);
+  }
